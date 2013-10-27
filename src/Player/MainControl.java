5c007ce2c40e8b852b5cc013e2851204f5a61763
DulinaArtem <artem.dulina@mail.ru>
2013-10-27 21:03:54 +0300

    add new classes

--- src/Player/MainControl.java
+++ src/Player/MainControl.java
@@ -1,11 +1,11 @@
 package Player;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Артем Дулина
- * Date: 27.10.13
- * Time: 20:39
- * To change this template use File | Settings | File Templates.
- */
+
 public class MainControl {
+    public static void main(String [] args) {
+        MusicCompositions comp = new MusicCompositions(100);
+        comp.addSong("God", "Dance", 100);
+        comp.addSong("God2", "Dance", 100);
+        System.out.println(comp);
+    }
 }
--- src/Player/MusicCompositions.java
+++ src/Player/MusicCompositions.java
@@ -1,11 +1,51 @@
 package Player;
+import java.io.*;
+import java.util.StringTokenizer;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Артем Дулина
- * Date: 27.10.13
- * Time: 20:39
- * To change this template use File | Settings | File Templates.
- */
 public class MusicCompositions {
+    protected Song[] store;
+    protected int countSongs;
+    protected int position = 0;
+
+    MusicCompositions(int count) {
+        countSongs = count;
+        store = new Song[countSongs];
+    }
+
+    MusicCompositions(String path) throws FileNotFoundException {
+        StringTokenizer st = new StringTokenizer();
+        File temp = new File(path);
+        BufferedReader bf = new BufferedReader(new InputStreamReader(new FileInputStream(temp)));
+        while(bf.readLine())
+    }
+
+    public void addSong(String songTitle, String ganre, int duration) {
+        store[position] = new Song(songTitle, ganre, duration);
+        position++;
+        countSongs = position;
+    }
+
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < countSongs; i++)
+            temp += store[i] + "\n";
+        return temp;
+    }
+
+    private class Song {
+        String songTitle;
+        String ganre;
+        int duration;
+
+        Song(String songTitle, String ganre, int duration) {
+            this.songTitle = songTitle;
+            this.ganre = ganre;
+            this.duration = duration;
+        }
+
+        public String toString() {
+            return songTitle + " " + ganre + " " + duration;
+        }
+    }
 }
+
