81c4254e4915178d1b46493096fd7fcba6552d73
DulinaArtem <artem.dulina@mail.ru>
2013-10-27 20:45:02 +0300

    git ig nore

---
+++ src/Player/MainControl.java
@@ -0,0 +1,11 @@
+package Player;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Артем Дулина
+ * Date: 27.10.13
+ * Time: 20:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainControl {
+}
